ed48bed40d68338c3482e5e19442f86e67eea7c2
natalliasdh <natallia.sdh@gmail.com>
2021-06-17 15:47:48 -0400

    JavaScript validation

---
+++ assets/scripts/script.js
@@ -0,0 +1,22 @@
+const emailInput = document.querySelector('#email');
+const submitButton = document.querySelector('#sendit');
+const warningDiv = document.querySelector("#warning");
+const formSubscribe = document.querySelector("#subscribe");
+const regEmail = /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$/;
+
+formSubscribe.addEventListener('submit', (event) => {
+    event.preventDefault();
+});
+
+submitButton.addEventListener('click', () => {
+
+    console.log(emailInput.value);
+
+    if (!emailInput.value.match(regEmail)) {
+        warningDiv.textContent = "Please, provide a valid email";
+
+    } else {
+        warningDiv.textContent = "";
+    }
+
+});
